1d81a508209e78cbf1dd596876c5d3de6fed3f51
MateusFigueiredo <figueiredo1497@gmail.com>
2022-05-04 21:25:49 -0300

    Criando o arquivo wakatime

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import ReactDOM from 'react-dom';
+
+function Lista() {
+    return (
+        <ul>
+            <li>Topico 2 build</li>
+            <li>Topico 2</li>
+            <li>Topico 3</li>
+        </ul>
+    );
+}
+
+const lista = Lista();
+ReactDOM.render(<p>olaolaola</p>, document.querySelector(".root"));
