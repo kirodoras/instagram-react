e496c874f4c8d43c7628218435bb7619aade4bcd
MateusFigueiredo <figueiredo1497@gmail.com>
2022-05-07 00:30:14 -0300

    Componentizing App

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
+
+export default function App (){
+    return (
+        <div class="pagina">
+            <Header />
+            <Main />
+            <Footer />
+        </div>
+    );
+}
--- src/index.js
+++ src/index.js
@@ -1,24 +1,5 @@
 import ReactDOM from 'react-dom';
 import React from 'react';
-import Header from './components/Header';
-import Main from './components/Main';
-
-function App() {
-    return (
-        <div class="pagina">
-            <Header />
-            <Main />
-            <footer>
-                <div class="footer-mobile">
-                    <ion-icon name="home"></ion-icon>
-                    <ion-icon name="search-outline"></ion-icon>
-                    <ion-icon name="add-circle-outline"></ion-icon>
-                    <ion-icon name="heart-outline"></ion-icon>
-                    <ion-icon name="person-outline"></ion-icon>
-                </div>
-            </footer>
-    </div>
-    );
-}
+import App from './components/App';
 
 ReactDOM.render(<App />, document.querySelector(".root"));
