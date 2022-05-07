02eac667e53c78e2da9a9de1bf1ccab7a802130b
MateusFigueiredo <figueiredo1497@gmail.com>
2022-05-07 03:15:39 -0300

    Componentizing Sidebar

--- src/components/SideBarFixed.js
+++ src/components/SideBarFixed.js
@@ -1,78 +1,71 @@
+const userInfo = {
+    urlUserImg: "assets/img-sidebar/catanacomics1.png",
+    userName: "catanacomics",
+    userDes: "Catana"
+};
+
+const suggestionUserInfo = [
+    {
+        url: "assets/img-sidebar/badvibesmemes1.png",
+        userName: "bad.vibes.memes"
+    },
+    {
+        url: "assets/img-sidebar/chibirdart1.png",
+        userName: "chibirdart"
+    },
+    {
+        url: "assets/img-sidebar/razoesparaacreditar1.png",
+        userName: "razoesparaacreditar"
+    },
+    {
+        url: "assets/img-sidebar/adorableanimals1.png",
+        userName: "adorable_animals"
+    },
+    {
+        url: "assets/img-sidebar/smallcutecats1.png",
+        userName: "smallcutecats"
+    },
+];
+
+function User() {
+    return (
+        <div class="user">
+            <img src={userInfo.urlUserImg} alt="Imagem do usuario" />
+            <div class="user-name">
+                <h3>{userInfo.userName}</h3>
+                <h4>{userInfo.userDes}</h4>
+            </div>
+        </div>
+    );
+}
+
+function CreateSuggestionUser(props) {
+    return (
+        <div class="suggestion-user">
+            <div class="suggestion-user-info">
+                <img src={props.url} alt={props.userName} />
+                <div class="suggestion-user-name">
+                    <h3>{props.userName}</h3>
+                    <h4>Segue você</h4>
+                </div>
+            </div>
+            <a href="https://www.driven.com.br/">
+                <p>Seguir</p>
+            </a>
+        </div>
+    );
+}
+
 export default function SideBarFixed() {
     return (
         <div class="sidebar-fixed">
-            <div class="user">
-                <img src="assets/img-sidebar/catanacomics1.png" alt="Imagem do usuario" />
-                <div class="user-name">
-                    <h3>catanacomics</h3>
-                    <h4>Catana</h4>
-                </div>
-            </div>
+            <User />
             <div class="suggestions">
                 <div class="suggestions-see-all">
                     <p>Sugestões para você</p>
                     <p>Ver tudo</p>
                 </div>
-                <div class="suggestion-user">
-                    <div class="suggestion-user-info">
-                        <img src="assets/img-sidebar/badvibesmemes1.png" alt="badvibesmemes1" />
-                        <div class="suggestion-user-name">
-                            <h3>bad.vibes.memes</h3>
-                            <h4>Segue você</h4>
-                        </div>
-                    </div>
-                    <a href="https://www.driven.com.br/">
-                        <p>Seguir</p>
-                    </a>
-                </div>
-                <div class="suggestion-user">
-                    <div class="suggestion-user-info">
-                        <img src="assets/img-sidebar/chibirdart1.png" alt="chibirdart1" />
-                        <div class="suggestion-user-name">
-                            <h3>chibirdart</h3>
-                            <h4>Segue você</h4>
-                        </div>
-                    </div>
-                    <a href="https://www.driven.com.br/">
-                        <p>Seguir</p>
-                    </a>
-                </div>
-                <div class="suggestion-user">
-                    <div class="suggestion-user-info">
-                        <img src="assets/img-sidebar/razoesparaacreditar1.png" alt="razoesparaacreditar1" />
-                        <div class="suggestion-user-name">
-                            <h3>razoesparaacreditar</h3>
-                            <h4>Catana</h4>
-                        </div>
-                    </div>
-                    <a href="https://www.driven.com.br/">
-                        <p>Seguir</p>
-                    </a>
-                </div>
-                <div class="suggestion-user">
-                    <div class="suggestion-user-info">
-                        <img src="assets/img-sidebar/adorableanimals1.png" alt="adorableanimals1" />
-                        <div class="suggestion-user-name">
-                            <h3>adorable_animals</h3>
-                            <h4>Segue você</h4>
-                        </div>
-                    </div>
-                    <a href="https://www.driven.com.br/">
-                        <p>Seguir</p>
-                    </a>
-                </div>
-                <div class="suggestion-user">
-                    <div class="suggestion-user-info">
-                        <img src="assets/img-sidebar/smallcutecats1.png" alt="smallcutecats1" />
-                        <div class="suggestion-user-name">
-                            <h3>smallcutecats</h3>
-                            <h4>Segue você</h4>
-                        </div>
-                    </div>
-                    <a href="https://www.driven.com.br/">
-                        <p>Seguir</p>
-                    </a>
-                </div>
+                {suggestionUserInfo.map(user => <CreateSuggestionUser url= {user.url} userName= {user.userName}></CreateSuggestionUser>)}
                 <p class="insta-about">Sobre • Ajuda • Imprensa • API • Carreiras • Privacidade • Termos • Localizações • Contas mais relevantes • Hashtags • Idioma </p>
                 <p class="insta-register">© 2022 INSTAGRAM FROM META</p>
             </div>
